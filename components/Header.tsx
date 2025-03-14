b2738340280c786bee86fb453c13bf4cfd7bb777
Meg <meghan@sbwell.com>
2025-03-14 14:17:15 -0700

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,5 +9,5 @@ export default function Header() {
   return (
     <header className="bg-blue-900 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-2xl font-bold">\
+        <Link href="/" className="text-2xl font-bold">
 
